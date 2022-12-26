58f24678741b33c824ab1b50cbf2f3d25bedbe5a
wyf-ACCEPT <define_wifi@163.com>
2022-12-26 17:00:04 +0800

    update hop

--- hop/src/processor.ts
+++ hop/src/processor.ts
@@ -2,32 +2,33 @@ import { scaleDown } from '@sentio/sdk/lib/utils/token'
 import { HopBridgeProcessor, HopBridgeContext } from './types/hopbridge'
 import { HopTokenSwapProcessor, HopTokenSwapContext, TokenSwapEvent } from './types/hoptokenswap'
 
-const saddleSwapMap: { [index: number]: [number, { [index: string]: string }] } = {
-  10: [29000000, {     // Optimism
-    'USDC': '0x3c0FFAca566fCcfD9Cc95139FEF6CBA143795963',
-  }],
-  137: [16000000, {    // Polygon
-    'USDC': '0x5C32143C8B198F392d01f8446b754c181224ac26',
-  }],
-  42161: [15000000, {   // Arbitrum
-    'USDC': '0x10541b07d8Ad2647Dc6cD67abd4c03575dade261',
-  }],
+const saddleSwapMap: { [index: number]: [number, [[string, string, number]]] } = {
+  10: [29000000, [     // Optimism
+    ['USDC', '0x3c0FFAca566fCcfD9Cc95139FEF6CBA143795963', 6],
+  ]],
+  137: [16000000, [    // Polygon
+    ['USDC', '0x5C32143C8B198F392d01f8446b754c181224ac26', 6],
+  ]],
+  42161: [15000000, [  // Arbitrum
+    ['USDC', '0x10541b07d8Ad2647Dc6cD67abd4c03575dade261', 6],
+  ]],
 }
 
-const handleTokenSwap = function (tokenName: string) {
-  return async function (event: TokenSwapEvent, ctx: HopTokenSwapContext) {
-    if (event.args.soldId.toBigInt() == 1n) {
-      var price = event.args.tokensSold.toNumber() / event.args.tokensBought.toNumber()
-    } else {
-      var price = event.args.tokensBought.toNumber() / event.args.tokensSold.toNumber()
-    }
-    ctx.meter.Gauge(tokenName + '_price').record(price)
+
+const handleBlock = function (tokenName: string, decimal: number) {
+  return async function (_: any, ctx: HopTokenSwapContext) {
+    const priceOrigin = scaleDown(await ctx.contract.calculateSwap(0, 1, 10n ** BigInt(decimal)), decimal)
+    const balanceOrigin = scaleDown(await ctx.contract.getTokenBalance(0), decimal)
+    const balanceHWrap = scaleDown(await ctx.contract.getTokenBalance(1), decimal)
+    ctx.meter.Gauge("price").record(priceOrigin, { "pair": tokenName + '_h' + tokenName })
+    ctx.meter.Gauge("pool").record(balanceOrigin, { "token": tokenName })
+    ctx.meter.Gauge("pool").record(balanceHWrap, { "token": 'h' + tokenName })
   }
 }
 
 for (var [chainId, [startBlock, tokens]] of Object.entries(saddleSwapMap)) {
-  for (var [tokenName, saddleAddr] of Object.entries(tokens)) {
+  for (var [tokenName, saddleAddr, decimal] of tokens) {
     HopTokenSwapProcessor.bind({ address: saddleAddr, network: Number(chainId), startBlock: startBlock })
-      .onEventTokenSwap(handleTokenSwap(tokenName))
+      .onBlock(handleBlock(tokenName, decimal))
   }
 }
