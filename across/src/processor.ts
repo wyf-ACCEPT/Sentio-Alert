e60e4895a705876821d870d4f8c6653f2254565c
wyf-ACCEPT <define_wifi@163.com>
2023-05-02 12:27:05 +0800

    acrossTo change contract addresses

--- across/src/processor.ts
+++ across/src/processor.ts
@@ -1,30 +1,48 @@
 import { chain, token } from '@sentio/sdk/lib/utils'
 import { AcrossToProcessor, AcrossToContext, FundsDepositedEvent, FilledRelayEvent } from './types/acrossto'
 
-const AcrossMap: { [index: number]: [string, [string, string, number][]] } = {
-  1: ["0x4D9079Bb4165aeb4084c526a32695dCfd2F77381", [
+// New address (2023/5/2)
+// Ethereum: 0x5c7BCd6E7De5423a257D81B442095A1a6ced35C5
+// Polygon: 0x9295ee1d8C5b022Be115A2AD3c30C72E34e7F096
+// Arbitrum: 0xe35e9842fceaCA96570B734083f4a58e8F7C5f2A
+// Optimism: 0x6f26Bf09B1C792e3228e5467807a900A503c0281
+
+const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
+  1: [[
+    "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381",
+    "0x5c7BCd6E7De5423a257D81B442095A1a6ced35C5",
+  ], [
     ["ETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", 18],
     ["USDC", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 6],
     ["DAI", "0x6B175474E89094C44Da98b954EedeAC495271d0F", 18],
   ]],     // Ethereum
 
-  10: ["0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9", [
+  10: [[
+    "0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9",
+    "0x6f26Bf09B1C792e3228e5467807a900A503c0281",
+  ], [
     ["ETH", "0x4200000000000000000000000000000000000006", 18],
     ["USDC", "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", 6],
     ["DAI", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", 18],
   ]],     // Optimism
 
-  137: ["0x69B5c72837769eF1e7C164Abc6515DcFf217F920", [
+  137: [[
+    "0x69B5c72837769eF1e7C164Abc6515DcFf217F920",
+    "0x9295ee1d8C5b022Be115A2AD3c30C72E34e7F096",
+  ], [
     ["ETH", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", 18],
     ["USDC", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", 6],
     ["DAI", "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063", 18],
   ]],     // Polygon
 
-  42161: ["0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C", [
-    ["ETH", "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", 18],
-    ["USDC", "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", 6],
-    ["DAI", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", 18],
-  ]],     // Arbitrum
+  42161: [
+    ["0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C",
+      "0xe35e9842fceaCA96570B734083f4a58e8F7C5f2A",
+    ], [
+      ["ETH", "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", 18],
+      ["USDC", "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", 6],
+      ["DAI", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", 18],
+    ]],     // Arbitrum
 }
 
 const EthPrice = 1200
@@ -57,15 +75,16 @@ const handleSwapInAcross = function (chainId: string, tokenName: string, decimal
   }
 }
 
-for (const [chainId, [poolAddr, tokenList]] of Object.entries(AcrossMap)) {
-  for (const [tokenName, tokenAddr, decimal] of tokenList) {
-    AcrossToProcessor.bind({ address: poolAddr, network: Number(chainId) })
-      .onEventFundsDeposited(
-        handleSwapOutAcross(chainId, tokenName, decimal),
-        AcrossToProcessor.filters.FundsDeposited(null, null, null, null, null, null, tokenAddr)
-      )
-      .onEventFilledRelay(
-        handleSwapInAcross(chainId, tokenName, decimal, tokenAddr)
-      )
-  }
+for (const [chainId, [poolList, tokenList]] of Object.entries(AcrossMap)) {
+  for (const poolAddr of poolList)
+    for (const [tokenName, tokenAddr, decimal] of tokenList) {
+      AcrossToProcessor.bind({ address: poolAddr, network: Number(chainId) })
+        .onEventFundsDeposited(
+          handleSwapOutAcross(chainId, tokenName, decimal),
+          AcrossToProcessor.filters.FundsDeposited(null, null, null, null, null, null, tokenAddr)
+        )
+        .onEventFilledRelay(
+          handleSwapInAcross(chainId, tokenName, decimal, tokenAddr)
+        )
+    }
 }
