870b02c898ebb2fa456565059793c3aa6a2ac3cc
wyf-ACCEPT <define_wifi@163.com>
2023-05-04 23:17:02 +0800

    update acrossTo

--- across/src/processor.ts
+++ across/src/processor.ts
@@ -1,5 +1,6 @@
 import { chain, token } from '@sentio/sdk/lib/utils'
-import { AcrossToProcessor, AcrossToContext, FundsDepositedEvent, FilledRelayEvent } from './types/acrossto'
+import { AcrossToOldContext, AcrossToOldProcessor, FundsDepositedEvent as FundsDepositedEventOld, FilledRelayEvent as FilledRelayEventOld } from './types/acrosstoold'
+import { AcrossToNewContext, AcrossToNewProcessor, FundsDepositedEvent as FundsDepositedEventNew, FilledRelayEvent as FilledRelayEventNew } from './types/acrosstonew'
 
 // New address (2023/5/2)
 // Ethereum: 0x5c7BCd6E7De5423a257D81B442095A1a6ced35C5
@@ -9,7 +10,7 @@ import { AcrossToProcessor, AcrossToContext, FundsDepositedEvent, FilledRelayEve
 
 const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   1: [[
-    // "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381",
+    "0x4D9079Bb4165aeb4084c526a32695dCfd2F77381",
     "0x5c7BCd6E7De5423a257D81B442095A1a6ced35C5",
   ], [
     ["ETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", 18],
@@ -18,7 +19,7 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ]],     // Ethereum
 
   10: [[
-    // "0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9",
+    "0xa420b2d1c0841415A695b81E5B867BCD07Dff8C9",
     "0x6f26Bf09B1C792e3228e5467807a900A503c0281",
   ], [
     ["ETH", "0x4200000000000000000000000000000000000006", 18],
@@ -27,7 +28,7 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ]],     // Optimism
 
   137: [[
-    // "0x69B5c72837769eF1e7C164Abc6515DcFf217F920",
+    "0x69B5c72837769eF1e7C164Abc6515DcFf217F920",
     "0x9295ee1d8C5b022Be115A2AD3c30C72E34e7F096",
   ], [
     ["ETH", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", 18],
@@ -36,7 +37,7 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ]],     // Polygon
 
   42161: [[
-    // "0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C",
+    "0xB88690461dDbaB6f04Dfad7df66B7725942FEb9C",
     "0xe35e9842fceaCA96570B734083f4a58e8F7C5f2A",
   ], [
     ["ETH", "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", 18],
@@ -45,11 +46,11 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ]],     // Arbitrum
 }
 
-const EthPrice = 1200
+const EthPrice = 1600
 
-const handleSwapOutAcross = function (chainId: string, tokenName: string, decimal: number) {
+const handleSwapOutAcrossOld = function (chainId: string, tokenName: string, decimal: number) {
   const chainName = chain.getChainName(chainId).toLowerCase()
-  return async function (event: FundsDepositedEvent, ctx: AcrossToContext) {
+  return async function (event: FundsDepositedEventOld, ctx: AcrossToOldContext) {
     var value = token.scaleDown(event.args.amount, decimal)
     if (tokenName == "ETH") value = value.multipliedBy(EthPrice)
     ctx.meter.Gauge("swapOutAmount").record(value, {
@@ -60,31 +61,32 @@ const handleSwapOutAcross = function (chainId: string, tokenName: string, decima
   }
 }
 
-const handleSwapInAcross = function (chainId: string, tokenName: string, decimal: number, tokenAddr: string) {
+const handleSwapOutAcrossNew = function (chainId: string, tokenName: string, decimal: number, tokenAddr: string) {
   const chainName = chain.getChainName(chainId).toLowerCase()
-  return async function (event: FilledRelayEvent, ctx: AcrossToContext) {
+  return async function (event: FundsDepositedEventNew, ctx: AcrossToNewContext) {
     var value = token.scaleDown(event.args.amount, decimal)
     if (tokenName == "ETH") value = value.multipliedBy(EthPrice)
-    if (event.args.destinationToken == tokenAddr) {
-      ctx.meter.Gauge("swapInAmount").record(value, {
-        "src": chain.getChainName(event.args.originChainId.toNumber()).toLowerCase(),
+    if (event.args.originToken == tokenAddr) {
+      ctx.meter.Gauge("swapOutAmount").record(value, {
+        "src": chainName,
         "token": tokenName,
-        "dst": chainName,
+        "dst": chain.getChainName(event.args.destinationChainId.toNumber()).toLowerCase(),
       })
     }
   }
 }
 
 for (const [chainId, [poolList, tokenList]] of Object.entries(AcrossMap)) {
-  for (const poolAddr of poolList)
-    for (const [tokenName, tokenAddr, decimal] of tokenList) {
-      AcrossToProcessor.bind({ address: poolAddr, network: Number(chainId) })
-        .onEventFundsDeposited(
-          handleSwapOutAcross(chainId, tokenName, decimal),
-          AcrossToProcessor.filters.FundsDeposited(null, null, null, null, null, null, tokenAddr)
-        )
-        .onEventFilledRelay(
-          handleSwapInAcross(chainId, tokenName, decimal, tokenAddr)
-        )
-    }
+  const [contractOld, contractNew] = poolList
+  for (const [tokenName, tokenAddr, decimal] of tokenList) {
+    AcrossToOldProcessor.bind({ address: contractOld, network: Number(chainId) })
+      .onEventFundsDeposited(
+        handleSwapOutAcrossOld(chainId, tokenName, decimal),
+        AcrossToOldProcessor.filters.FundsDeposited(null, null, null, null, null, null, tokenAddr)
+      )
+    AcrossToNewProcessor.bind({ address: contractNew, network: Number(chainId) })
+      .onEventFundsDeposited(
+        handleSwapOutAcrossNew(chainId, tokenName, decimal, tokenAddr)
+      )
+  }
 }
