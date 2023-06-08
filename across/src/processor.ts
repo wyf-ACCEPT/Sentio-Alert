0e8bf55e46d12cf730ee2332643385247a09fc32
wyf-ACCEPT <define_wifi@163.com>
2023-06-08 12:09:32 +0800

    across +USDT; cbridge/multichain +zksync era

--- across/src/processor.ts
+++ across/src/processor.ts
@@ -15,6 +15,7 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ], [
     ["ETH", "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2", 18],
     ["USDC", "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48", 6],
+    ["USDT", "0xdAC17F958D2ee523a2206206994597C13D831ec7", 6],
     ["DAI", "0x6B175474E89094C44Da98b954EedeAC495271d0F", 18],
   ]],     // Ethereum
 
@@ -24,6 +25,7 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ], [
     ["ETH", "0x4200000000000000000000000000000000000006", 18],
     ["USDC", "0x7F5c764cBc14f9669B88837ca1490cCa17c31607", 6],
+    ["USDT", "0x94b008aA00579c1307B0EF2c499aD98a8ce58e58", 6],
     ["DAI", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", 18],
   ]],     // Optimism
 
@@ -33,6 +35,7 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ], [
     ["ETH", "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619", 18],
     ["USDC", "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174", 6],
+    ["USDT", "0xc2132D05D31c914a87C6611C10748AEb04B58e8F", 6],
     ["DAI", "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063", 18],
   ]],     // Polygon
 
@@ -42,6 +45,7 @@ const AcrossMap: { [index: number]: [string[], [string, string, number][]] } = {
   ], [
     ["ETH", "0x82aF49447D8a07e3bd95BD0d56f35241523fBab1", 18],
     ["USDC", "0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8", 6],
+    ["USDT", "0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9", 6],
     ["DAI", "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1", 18],
   ]],     // Arbitrum
 }
--- multichain/src/processor.ts
+++ multichain/src/processor.ts
@@ -14,54 +14,54 @@ const addressMap: { [index: number]: [string[], [string, string, number][]] } = 
       ['anyUSDC', '0x7ea2be2df7ba6e54b1a9c70676f668455e329d29', 6],
       ['anyUSDT', '0x22648c12acd87912ea1710357b1302c6a4154ebc', 6],
     ]],
-  56: [   // BNB Chain
-    [
-      '0xd1C5966f9F5Ee6881Ff6b261BBeDa45972B1B5f3'
-    ], [
-      ['anyETH', '0xdebb1d6a2196f2335ad51fbde7ca587205889360', 18],
-      ['anyUSDC', '0x8965349fb649a33a30cbfda057d8ec2c48abe2a2', 18],
-      ['anyUSDT', '0xedf0c420bc3b92b961c6ec411cc810ca81f5f21a', 18],
-    ]
-  ],
-  137: [  // Polygon
-    [
-      '0x4f3Aff3A747fCADe12598081e80c6605A8be192F',
-    ], [
-      ['anyETH', '0xbD83010eB60F12112908774998F65761cf9f6f9a', 18],
-      ['anyUSDC', '0xd69b31c3225728CC57ddaf9be532a4ee1620Be51', 6],
-      ['anyUSDT', '0xE3eeDa11f06a656FcAee19de663E84C7e61d3Cac', 6],
-      ['anyDAI', '0x9b17bAADf0f21F03e35249e0e59723F34994F806', 18],
-    ]
-  ],
-  10: [  // Optimisim
-    [
-      '0xDC42728B0eA910349ed3c6e1c9Dc06b5FB591f98'
-    ], [
-      ['anyETH', '0x965f84D915a9eFa2dD81b653e3AE736555d945f4', 18],
-      ['anyUSDC', '0xf390830DF829cf22c53c8840554B98eafC5dCBc2', 6],
-    ]
-  ],
-  42161: [  // Arbitrum
-    [
-      '0xC931f61B1534EB21D8c11B24f3f5Ab2471d4aB50',
-      '0x650Af55D5877F289837c30b94af91538a7504b76'
-    ], [
-      ['anyETH', '0x1Dd9e9e142f3f84d90aF1a9F2cb617C7e08420a4', 18],
-      ['anyUSDC', '0x3405A1bd46B85c5C029483FbECf2F3E611026e45', 6],
-      ['anyUSDT', '0x05e481B19129B560E921E487AdB281E70Bdba463', 6],
-      ['anyDAI', '0xaef9E3e050D0Ef060cdfd5246209B0B6BB66060F', 18],
-    ]
-  ],
-  43114: [  // Avalanche
-    [
-      '0xB0731d50C681C45856BFc3f7539D5f61d4bE81D8',
-    ], [
-      ['anyETH', '0x7D09a42045359Aa85488bC07D0ADa83E22d50017', 18],
-      ['anyUSDC', '0xcc9b1F919282c255eB9AD2C0757E8036165e0cAd', 6],
-      ['anyUSDT', '0x94977c9888F3D2FAfae290d33fAB4a5a598AD764', 6],
-      ['anyDAI', '0xd4143E8dB48a8f73afCDF13D7B3305F28DA38116', 18],
-    ]
-  ],
+  // 56: [   // BNB Chain
+  //   [
+  //     '0xd1C5966f9F5Ee6881Ff6b261BBeDa45972B1B5f3'
+  //   ], [
+  //     ['anyETH', '0xdebb1d6a2196f2335ad51fbde7ca587205889360', 18],
+  //     ['anyUSDC', '0x8965349fb649a33a30cbfda057d8ec2c48abe2a2', 18],
+  //     ['anyUSDT', '0xedf0c420bc3b92b961c6ec411cc810ca81f5f21a', 18],
+  //   ]
+  // ],
+  // 137: [  // Polygon
+  //   [
+  //     '0x4f3Aff3A747fCADe12598081e80c6605A8be192F',
+  //   ], [
+  //     ['anyETH', '0xbD83010eB60F12112908774998F65761cf9f6f9a', 18],
+  //     ['anyUSDC', '0xd69b31c3225728CC57ddaf9be532a4ee1620Be51', 6],
+  //     ['anyUSDT', '0xE3eeDa11f06a656FcAee19de663E84C7e61d3Cac', 6],
+  //     ['anyDAI', '0x9b17bAADf0f21F03e35249e0e59723F34994F806', 18],
+  //   ]
+  // ],
+  // 10: [  // Optimisim
+  //   [
+  //     '0xDC42728B0eA910349ed3c6e1c9Dc06b5FB591f98'
+  //   ], [
+  //     ['anyETH', '0x965f84D915a9eFa2dD81b653e3AE736555d945f4', 18],
+  //     ['anyUSDC', '0xf390830DF829cf22c53c8840554B98eafC5dCBc2', 6],
+  //   ]
+  // ],
+  // 42161: [  // Arbitrum
+  //   [
+  //     '0xC931f61B1534EB21D8c11B24f3f5Ab2471d4aB50',
+  //     '0x650Af55D5877F289837c30b94af91538a7504b76'
+  //   ], [
+  //     ['anyETH', '0x1Dd9e9e142f3f84d90aF1a9F2cb617C7e08420a4', 18],
+  //     ['anyUSDC', '0x3405A1bd46B85c5C029483FbECf2F3E611026e45', 6],
+  //     ['anyUSDT', '0x05e481B19129B560E921E487AdB281E70Bdba463', 6],
+  //     ['anyDAI', '0xaef9E3e050D0Ef060cdfd5246209B0B6BB66060F', 18],
+  //   ]
+  // ],
+  // 43114: [  // Avalanche
+  //   [
+  //     '0xB0731d50C681C45856BFc3f7539D5f61d4bE81D8',
+  //   ], [
+  //     ['anyETH', '0x7D09a42045359Aa85488bC07D0ADa83E22d50017', 18],
+  //     ['anyUSDC', '0xcc9b1F919282c255eB9AD2C0757E8036165e0cAd', 6],
+  //     ['anyUSDT', '0x94977c9888F3D2FAfae290d33fAB4a5a598AD764', 6],
+  //     ['anyDAI', '0xd4143E8dB48a8f73afCDF13D7B3305F28DA38116', 18],
+  //   ]
+  // ],
   250: [  // Fantom
     [
       '0x1CcCA1cE62c62F7Be95d4A67722a8fDbed6EEcb4',
@@ -70,6 +70,14 @@ const addressMap: { [index: number]: [string[], [string, string, number][]] } = 
       ['anyUSDC', '0x95bf7E307BC1ab0BA38ae10fc27084bC36FcD605', 6],
       ['anyUSDT', '0x2823D10DA533d9Ee873FEd7B16f4A962B2B7f181', 6],
       ['anyDAI', '0xd652776dE7Ad802be5EC7beBfafdA37600222B48', 18],
+    ]
+  ],
+  324: [  // Zksync Era
+    [
+      '0xff7104537F33937c66Ac0a65609EB8364Be75c7A',
+    ], [
+      ['anyETH', '0x7BcD44c0B91bE28827426f607424E1A8A02d4E69', 18],
+      ['anyUSDC', '0x5f3268A22A624AE81281D47A247dAb8F7105f2F9', 6],
     ]
   ]
 }
